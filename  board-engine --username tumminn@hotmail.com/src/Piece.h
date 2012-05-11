2f1b895f21978d609215ca2fbf338a18edf0c0c6
bjozzi@gmail.com <bjozzi@gmail.com@98a3e539-334e-a370-5b13-eab1aa3869ae>
2012-05-11 11:46:02 +0000

    Made comments on the BOARD_H, GAMEEXCEPTION_H, MOVE_H, PIECE_H

---  board-engine --username tumminn@hotmail.com/src/Piece.h
+++  board-engine --username tumminn@hotmail.com/src/Piece.h
@@ -4,20 +4,43 @@
 #include <string>
 #include <cctype>
 
+/** \brief A class for handeling the pieces.
+ *
+ * A class for each piece that the board class uses for pieces on the board.
+ */
 class Piece
 {
-    public:
-        Piece(std::string name = ".", int owner = -1)
-            :m_name(name),m_owner(owner)
-            {};
-        virtual ~Piece();
+public:
+    /** \brief The piece constructor.
+     *
+     * Constructs a Piece with the given parameters
+     * @param name string argument that has a default value "."
+     * @param owner int argument that has a default value -1
+     */
+    Piece(std::string name = ".", int owner = -1)
+        :m_name(name),m_owner(owner)
+    {};
+    virtual ~Piece();
 
-        int getOwner();
-        std::string getName();//Always returns as lower case
-    protected:
-    private:
-        std::string m_name;
-        int m_owner;
+    /** \brief Getter for the owner variable.
+     *
+     * Normal getter function which returns an int variable that denotes which player
+     * owns the piece. 0 for player 1 and 1 for player 2.
+     * @return: owner of the piece
+     */
+    int getOwner();
+
+    /** \brief Getter for the name variable.
+     *
+     * Getter for the variable string name, turns it to lower case and returns it.
+     * @return: name of piece, always a lower case.
+     */
+    std::string getName();
+
+protected:
+private:
+    std::string m_name; /**< name of the piece, pawn, king etc. */
+    int m_owner; /**< owner of the piece, 0 for player 1 and 1 for player 2. */
 };
 
 #endif // PIECE_H
