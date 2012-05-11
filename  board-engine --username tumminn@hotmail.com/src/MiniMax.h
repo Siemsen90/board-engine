9f99d14bf9e900fc1caac5428078655b0dfa8e16
bjozzi@gmail.com <bjozzi@gmail.com@98a3e539-334e-a370-5b13-eab1aa3869ae>
2012-05-11 13:55:37 +0000

    Commented Game.h MiniMax.h

---  board-engine --username tumminn@hotmail.com/src/MiniMax.h
+++  board-engine --username tumminn@hotmail.com/src/MiniMax.h
@@ -9,16 +9,34 @@
 #include <stdlib.h>
 #include <time.h>
 
+/** \brief Performs minimax on a game.
+ *
+ * Minimax uses a game object and finds all the legal moves and executes the minimax algorithm on that.
+ */
 class MiniMax
 {
     public:
         MiniMax();
         virtual ~MiniMax();
 
+        /** \brief Runs the minimax algorithm.
+         *
+         * Run uses a game object and finds all the legal moves and executes the minimax algorithm on that
+         * to find the best move available.
+         * @param *game pointer to a game that minimax is performing on
+         */
         void run(Game *game);
 
     protected:
     private:
+        /** \brief The actual miniMax algorithm.
+         *
+         * The actual minimax algorithm that run calls. and gets in a variable depth that denotes on what depth
+         * it goes.
+         * @param depth integer parameter denoting on what depth minimax should go
+         * @param *game pointer to a game that minimax is performing on
+         * @return integer value which has the best value.
+         */
         int miniMax(int depth, Game *game);
 };
 
